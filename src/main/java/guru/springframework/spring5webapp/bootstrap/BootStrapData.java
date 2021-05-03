b5faf2a61365dbeec01b8ce10999dee21a4d9466
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-05-03 15:15:22 +0200

    add publisher entity: assignment2: section 2

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,9 +1,11 @@
 package guru.springframework.spring5webapp.bootstrap;
 
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,16 +13,19 @@ import org.springframework.stereotype.Component;
 public class BootStrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         Author matteo = new Author("Matteo", "De Kerpel");
         Book bio = new Book("Matteo in 2 words", "123123");
+
         matteo.getBooks().add(bio);
         bio.getAuthors().add(matteo);
 
@@ -37,5 +42,10 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
+
+        Publisher publisher1 = new Publisher("test", "test", "test", "test", 17474);
+        publisherRepository.save(publisher1);
+
+        System.out.println("Number of publishers: " + publisherRepository.count());
     }
 }
