4979083966432d345ee4d124698fbdffcc9ac96b
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-05-05 10:40:53 +0200

    finished assignment 3, section 2

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -17,8 +17,7 @@ public class Author {
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
-    public Author() {
-    }
+    public Author() { }
 
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
